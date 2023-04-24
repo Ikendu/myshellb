6f5731d524a2a10b8c8746390b3d3a04b8a8b498
aniede <ndubest56@gmail.com>
2023-04-24 16:33:40 +0100

    redoing simple shell

---
+++ shell_basic.c
@@ -0,0 +1,15 @@
+#ifndef SHELL_BASIC_C_INCLUDED
+#define SHELL_BASIC_C_INCLUDED
+
+int loop()
+{
+    char *arg;
+    char **piont;
+    int status = 1;
+
+    line = read_line();
+
+}
+
+
+#endif // SHELL_BASIC_C_INCLUDED
