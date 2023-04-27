5e7da6bb9d17f3fd99dcb081e5af262fa2071762
Chibundu <ndubest56@gmail.com>
2023-04-27 09:50:06 +0100

    analysing process id

---
+++ fork.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int a = 60;
+
+	printf("my current PID is %d\n", getpid());
+
+	pid_t childp = fork();
+	printf("%d\n", a);
+
+	printf("my parent PID is %d\n my current PID is %d\n", getppid(), getpid());
+        printf("Im done printing\n");
+
+	return (0);
+}
+
+
+
